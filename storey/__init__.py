1fb0f5f69a2d0ff06ee2cb737112b3a9fdfe26ff
Miki Tebeka <miki.tebeka@gmail.com>
2020-06-15 12:21:43 +0300

    no .gitignore & setup.py

--- storey/__init__.py
+++ storey/__init__.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+__version__ = '0.1.0'
+
 
 from .flow import (  # noqa: F401
     Filter, FilterMap, FlatMap, Flow, FlowException, Map, Reduce, Source,
